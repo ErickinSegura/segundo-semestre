cd5b912dabca27696753f89f7441883956c12636
ErickinSegura <A01613821@tec.mx>
2023-06-09 08:33:40 -0600

    Pez arreglado

--- Modelacion de la Ingenieria con Matematica Computacional/Reto/modules/listen.py
+++ Modelacion de la Ingenieria con Matematica Computacional/Reto/modules/listen.py
@@ -1,18 +1,24 @@
 import speech_recognition as sr
+import pygame
 
 r = sr.Recognizer()
+pygame.init()
+pygame.mixer.init()
+
 
 class Listener:
-    def listen(self):
-        with sr.Microphone() as source:    
-            print("Diga algo:")
-            audio = r.listen(source)
-            print("Reconociendo...")
-            try:
-                audio_text = r.recognize_google(audio, language='es-ES')
-                print("Texto: {}".format(audio_text))
-                return audio_text
+    def listen(self):   
+            try:    
+                with sr.Microphone() as source:
+                        sonido_fondo = pygame.mixer.Sound("placeholder.wav")
+                        pygame.mixer.Sound.play(sonido_fondo)
+                        audio = r.listen(source)
+                        audio_text = r.recognize_google(audio, language='es-ES')
+                        print("Texto: {}".format(audio_text))
+                        return audio_text.upper()
+                                  
             except:
                 print("Lo siento, no te entiendo")
+
 
 
